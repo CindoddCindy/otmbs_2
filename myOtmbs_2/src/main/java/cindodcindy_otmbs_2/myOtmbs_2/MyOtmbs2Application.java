41f9040ffd4c0bd85e36f7b6f0f428f9bfc8345c
CindoddCindy <cindodcindy@gmail.com>
2021-03-13 22:59:44 +0700

    folder

---
+++ myOtmbs_2/src/main/java/cindodcindy_otmbs_2/myOtmbs_2/MyOtmbs2Application.java
@@ -0,0 +1,13 @@
+package cindodcindy_otmbs_2.myOtmbs_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOtmbs2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOtmbs2Application.class, args);
+	}
+
+}
